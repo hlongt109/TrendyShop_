2aac91549e8072a4b278f9feec017220f06a7d1e
hlongt109 <hlong109.it@gmail.com>
2024-02-05 00:00:53 +0700

    long commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
     namespace = "com.trendyshopteam.trendyshop"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.trendyshopteam.trendyshop"
@@ -39,4 +40,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // firebase
+    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation( "com.google.firebase:firebase-storage")
+    implementation( "com.google.firebase:firebase-database")
+    implementation("com.google.firebase:firebase-auth")
+    implementation ("com.google.firebase:firebase-firestore")
+    // animation
+    implementation ("com.airbnb.android:lottie:6.3.0")
 }
