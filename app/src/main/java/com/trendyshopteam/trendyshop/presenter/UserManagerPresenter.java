569ef76bd9beb10c62f82e65222e6aa79b45a1b1
hlongt109 <hlong109.it@gmail.com>
2024-03-05 22:44:04 +0700

    long commit user manager

--- app/src/main/java/com/trendyshopteam/trendyshop/presenter/UserManagerPresenter.java
+++ app/src/main/java/com/trendyshopteam/trendyshop/presenter/UserManagerPresenter.java
@@ -1,10 +1,8 @@
 package com.trendyshopteam.trendyshop.presenter;
 
-import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.PopupMenu;
 
